61e4932dd80516aeec4d8f45818d44a09056f760
Chris Ware <c.p.ware@hotmail.com>
2023-10-09 15:41:19 -0700

    Register Form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -12,7 +12,7 @@ const RegisterPage = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      //
+      console.log(name, email, password);
     } catch (err) {
       console.log(err);
       setLoading(false);
@@ -26,6 +26,36 @@ const RegisterPage = () => {
         <div className='row d-flex justify-content-center align-items-center vh-100'>
           <div className='col-lg-5 shadow bg-light p-5'>
             <h2 className='mb-4 text-center'>Register</h2>
+
+            <form onSubmit={handleSubmit}>
+              <input
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className='form-control mb-4'
+                placeholder='Enter your name'
+              />
+              <input
+                type='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className='form-control mb-4'
+                placeholder='Enter your email'
+              />
+              <input
+                type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className='form-control mb-4'
+                placeholder='Enter your password'
+              />
+              <button
+                className='btn btn-primary btn-raised'
+                disabled={loading || !name || !email || !password}
+              >
+                {loading ? "Please Wait..." : "Submit"}
+              </button>
+            </form>
           </div>
         </div>
       </div>
