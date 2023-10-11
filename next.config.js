43b97fe7119ecd4a2b0af98b23fdcfb2aa327b83
Chris Ware <c.p.ware@hotmail.com>
2023-10-11 14:26:07 -0700

    deployment fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-const config = require("./config.ts");
+import { DB_URI as _DB_URI, API as _API, NEXTAUTH_SECRET as _NEXTAUTH_SECRET, GOOGLE_CLIENT_ID as _GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET as _GOOGLE_CLIENT_SECRET } from "./config.ts";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DB_URI: config.DB_URI,
-    API: config.API,
-    NEXTAUTH_SECRET: config.NEXTAUTH_SECRET,
-    GOOGLE_CLIENT_ID: config.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: config.GOOGLE_CLIENT_SECRET,
+    DB_URI: _DB_URI,
+    API: _API,
+    NEXTAUTH_SECRET: _NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: _GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: _GOOGLE_CLIENT_SECRET,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
