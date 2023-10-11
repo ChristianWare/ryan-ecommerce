9abc3bbd8bca3423969299fa4cece054a5f18c6d
Chris Ware <c.p.ware@hotmail.com>
2023-10-11 14:49:39 -0700

    .env file setup

--- next.config.js
+++ next.config.js
@@ -1,17 +1,36 @@
-const {
-  DB_URI: _DB_URI,
-  API: _API,
-  NEXTAUTH_SECRET: _NEXTAUTH_SECRET,
-  GOOGLE_CLIENT_ID: _GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: _GOOGLE_CLIENT_SECRET,
-} = require("./config.ts");
+// const {
+//   DB_URI: _DB_URI,
+//   API: _API,
+//   NEXTAUTH_SECRET: _NEXTAUTH_SECRET,
+//   GOOGLE_CLIENT_ID: _GOOGLE_CLIENT_ID,
+//   GOOGLE_CLIENT_SECRET: _GOOGLE_CLIENT_SECRET,
+// } = require("./config.ts");
 
+// module.exports = {
+//   env: {
+//     DB_URI: _DB_URI,
+//     API: _API,
+//     NEXTAUTH_SECRET: _NEXTAUTH_SECRET,
+//     GOOGLE_CLIENT_ID: _GOOGLE_CLIENT_ID,
+//     GOOGLE_CLIENT_SECRET: _GOOGLE_CLIENT_SECRET,
+//   },
+// };
+
+/** @type {import("next").NextConfig} */
 module.exports = {
-  env: {
-    DB_URI: _DB_URI,
-    API: _API,
-    NEXTAUTH_SECRET: _NEXTAUTH_SECRET,
-    GOOGLE_CLIENT_ID: _GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: _GOOGLE_CLIENT_SECRET,
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  experimental: {
+    serverActions: true,
+  },
+  
 };
