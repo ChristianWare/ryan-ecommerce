2112877d7de2186c4dfb216d60e3d6b68d844293
Chris Ware <c.p.ware@hotmail.com>
2023-10-11 19:11:10 -0700

    List Categories API

--- app/api/admin/category/route.ts
+++ app/api/admin/category/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import dbConnect from "@/utils/dbConnect";
 import Category from "../../../../models/category";
 import slugify from "slugify";
 
-export async function POST(req) {
+export async function POST(req: NextRequest) {
   await dbConnect();
   const body = await req.json();
   const { name } = body;
@@ -11,7 +11,17 @@ export async function POST(req) {
   try {
     const category = await Category.create({ name, slug: slugify(name) });
     return NextResponse.json(category);
-  } catch (err) {
+  } catch (err: any) {
+    return NextResponse.json(err.message, { status: 500 });
+  }
+}
+
+export async function GET() {
+  await dbConnect();
+  try {
+    const categories = await Category.find({}).sort({ createdAt: -1 });
+    return NextResponse.json(categories);
+  } catch (err: any) {
     return NextResponse.json(err.message, { status: 500 });
   }
 }
